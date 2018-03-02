3b8af4dd63fafd99c36dd0a4cb52752dfad058cf
xMoad <mouad.zaid@gmail.com>
2018-03-02 20:43:49 +0100

    Create bitcoin_ar.ts
    Commit file in myBranch, not finish yet.

---
+++ src/qt/locale/bitcoin_ar.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About VERGE</source>
+        <translation>حول البرنامج</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;VERGE&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;VERGE&lt;/b&gt; version</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Copyright © 2014-2018 The Verge developers</source>
+        <translation>حقوق النشرمحفوظة  © 2014-2018   مطوري Verge </translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>
+This is experimental software.
+​
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+​
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.</source>
+        <translation>
+.هذا البرنامج هو برنامج تجريبي
+​
+موزع تحت الرخصة MIT/X11, انظر الملف المصاحب COPYING او http://www.opensource.org/licenses/mit-license.php.
+​
+يتضمن هذا المنتج البرمجيات التي تم تطويرها في اطار OpenSSL Project لاستخدامها في مجموعة أدوات http://www.openssl.org/) OpenSSL ToolKit) وبرنامج التشفير لمطوره Eric Young (eay@cryptosoft.com)a و برنامج UPnP لمطوره Thomas Bernard.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>دليل العناوين</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Double-click to edit address or label</source>
+        <translation>انقر لتحرير العنوان أو التصنيف</translation>
+    </message>
+        <source>Loading wallet...</source>
+        <translation>Loading wallet...</translation>
+    </message>
+    <message>
+        <location line="-39"/>
+        <source>Cannot downgrade wallet</source>
+        <translation>Cannot downgrade wallet</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Cannot initialize keypool</source>
+        <translation>Cannot initialize keypool</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Cannot write default address</source>
+        <translation>Cannot write default address</translation>
+    </message>
+    <message>
+        <location line="+46"/>
+        <source>Rescanning...</source>
+        <translation>Rescanning...</translation>
+    </message>
+    <message>
+        <location line="-40"/>
+        <source>Done loading</source>
+        <translation>Done loading</translation>
+    </message>
+    <message>
+        <location line="+64"/>
+        <source>To use the %s option</source>
+        <translation>To use the %s option</translation>
+    </message>
+    <message>
+        <location line="-150"/>
+        <source>%s, you must set a rpcpassword in the configuration file:
+ %s
+It is recommended you use the following random password:
+rpcuser=bitcoinrpc
+rpcpassword=%s
+(you do not need to remember this password)
+If the file does not exist, create it with owner-readable-only file permissions.
+</source>
+        <translation>%s, you must set a rpcpassword in the configuration file:
+ %s
+It is recommended you use the following random password:
+rpcuser=bitcoinrpc
+rpcpassword=%s
+(you do not need to remember this password)
+If the file does not exist, create it with owner-readable-only file permissions.
+</translation>
+    </message>
+    <message>
+        <location line="+91"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location line="-30"/>
+        <source>You must set rpcpassword=&lt;password&gt; in the configuration file:
+%s
+If the file does not exist, create it with owner-readable-only file permissions.</source>
+        <translation>You must set rpcpassword=&lt;password&gt; in the configuration file:
+%s
+If the file does not exist, create it with owner-readable-only file permissions.</translation>
+    </message>
+</context>
+</TS>
+
